f45f86248afb270b194ce463ce3f5f115cc62752
Julien Fitzpatrick <jfitzpatrick@artemisag.com>
2020-08-26 21:20:37 -0700

    Very basic setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [railsQuery, setRailsQuery] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [results, setResults] = useState([])
+  const handleChange = (event) => {
+    setRailsQuery(event.target.value)
+  }
+
+  const submitRailsQuery = async () => {
+    setIsLoading(true)
+    const searchString = `http://localhost:3000/clue_queries?query_string=${railsQuery}`
+    try {
+      const response = await fetch(searchString)
+      const result = await response.json()
+      setResults(result.data)
+    } catch (err) {
+      console.error(err)
+      setError(err)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type='search' value={railsQuery} onChange={handleChange} />
+      <button onClick={submitRailsQuery}>Submit</button>
+      { isLoading &&
+        <span>Loading...</span>
+      }
+      { error &&
+        <span>An error occurred: {error}</span>
+      }
+      <ul>
+        { Array.isArray(results) && results.length > 0 && results.map((result, index) => {
+            return (<li key={`result-${index}`}>{result.attributes.name}</li>)
+          })
+        }
+        { !Array.isArray(results) && Object.keys(results).length > 0 &&
+          <li>{results.attributes.name}</li>
+        }
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default App
